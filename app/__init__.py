56dc4f152a8fb56820418d169cec04aa83e52197
Lidiia Verkhovskaia <lidija.inshina.yandex.ru>
2024-07-27 13:12:01 +0400

    feat: style.css

---
+++ app/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from config import Config
+
+# Инициализация расширения для работы с базой данных
+db = SQLAlchemy()
+# Инициализация менеджера для управления сессиями пользователей
+login = LoginManager()
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    # Загрузка конфигурации из класса Config
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    login.init_app(app)
+
+    # Импорт и регистрация блупринтов
+    from app.routes import bp
+    app.register_blueprint(bp)
+
+    with app.app_context():
+        db.create_all()
+
+    return app
---
+++ config.py
@@ -0,0 +1,4 @@
+class Config:
+    SECRET_KEY = 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
